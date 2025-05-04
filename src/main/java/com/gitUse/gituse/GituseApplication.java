3347b9d461ae86661bcdcfe95a84ced9d183f8cf
rajeev.70809518rajeev@gmail.com <rajeevknp7080@gmail.com>
2025-05-04 11:17:24 +0530

    initial commit

---
+++ src/main/java/com/gitUse/gituse/GituseApplication.java
@@ -0,0 +1,13 @@
+package com.gitUse.gituse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GituseApplication {
+int s = 10;
+	public static void main(String[] args) {
+		SpringApplication.run(GituseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitUse/gituse/GituseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitUse.gituse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GituseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
